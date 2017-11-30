506f8056eba8ceba2332988fb82c28e6c55364ac
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-11-30 18:18:37 -0500

    use 1. instead of 1./10 to avoid floating point shenanigans

--- test/fearnhead.jl
+++ test/fearnhead.jl
@@ -51,7 +51,7 @@
         @test cut/tot ≈ 1/10
 
         # when weights are all equal or zero, also resample all with weight 1/N
-        ws = [i > 10 ? 0. : 1/10 for i in 1:20]
+        ws = [i > 10 ? 0. : 1. for i in 1:20]
         keep, cut, tot = cutoff(ws, 10)
         @test isapprox(cutoff_normalized(ws, 10)[2], cut/tot, atol=1e-10)
         @test isapprox(cutoff_while(ws, 10)[2], cut/tot, atol=1e-10)
