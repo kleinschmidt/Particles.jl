90c13a3b8a5b264fac9385405b9f56efb25cb083
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-01-19 11:56:35 -0500

    also track assignment in particle

--- src/particle2.jl
+++ src/particle2.jl
@@ -4,6 +4,7 @@ abstract type AbstractParticle end
 struct Particle{P,S,A} <: AbstractParticle
     components::Vector{Component{P,S}}
     ancestor::A
+    assignment::Int
     weight::Float64
 end
 
@@ -20,15 +21,18 @@ end
 
 Base.showcompact(io::IO, p::Particle) = print(io, "$(length(p.components))-Particle")
 
-Particle(params::NTuple{4,<:Real}...) = Particle([Component.(params)...], nothing, 1.0)
+Particle(params::NTuple{4,<:Real}...) = Particle([Component.(params)...], nothing, 0, 1.0)
 
 function fit(p::Particle, y::Float64, x::Int)
     comps = copy(p.components)
     old_llhood = marginal_log_lhood(comps[x])
     comps[x] = add(comps[x], y)
     new_llhood = marginal_log_lhood(comps[x])
-    Particle(comps, p, p.weight * exp(new_llhood - old_llhood))
+    Particle(comps, p, x, p.weight * exp(new_llhood - old_llhood))
 end
 
 putatives(p::Particle, y::Float64) = [fit(p, y, x) for x in eachindex(p.components)]
 
+weight(p::Particle) = p.weight
+# now we have a problem: need to be able to change weight of particle...
+# weight!(p::Particle, w::Float64) = 
