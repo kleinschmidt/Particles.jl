c3f329ad4cb9f4fc5491bcadd3af4622b977ea2b
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-01-25 15:44:29 -0500

    also run chen-liu algorithm in ESS, and save results as JLD2

--- test/ess.jl
+++ test/ess.jl
@@ -8,9 +8,9 @@ addprocs(11)
     using Distributions: MixtureModel
     using ConjugatePriors: NormalInverseGamma, NormalInverseChisq
 
-    function m_m2(ps::FearnheadParticles)
-        ns = ncomponents.(ps.particles)
-        ws = Weights(weight.(ps.particles))
+    function m_m2(ps::ParticleFilter)
+        ns = ncomponents.(particles(ps))
+        ws = Weights(weight.(particles(ps)))
         (mean(ns, ws), var(ns, ws))
     end
 
@@ -52,11 +52,15 @@ for σ in [0.5, 2.5]
                           [1/2, 1/6, 1/3])
         x = rand(mm, n_dat)
         fc = ess(() -> m_m2(fit!(FearnheadParticles(n_samp, prior, α), x, false)))
+        cl = ess(() -> m_m2(fit!(ChenLiuParticles(n_samp, prior, α, 50.), x, false)))
         gs = ess(() -> m_m2(GibbsCRP(prior, α, x), n_samp, burnin=500))
-        @show μ, σ, fc, gs
-        push!(results, (μ, σ, fc, gs))
+        @show μ, σ, fc, cl, gs
+        push!(results, (μ, σ, fc, cl, gs))
     end
 end
+
+using JLD2
+@save "ess_results_$(DateTime(now())).jld2" results 
 
 # run using ESS formula from Fearnhead(2004)
 # (μ, σ, fc, gs) = (0.5, 0.5, 707.4197999272996, 250.4401078254187)
