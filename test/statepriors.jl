15c82f78732806020d4db1c0a1f005f9d02d6d33
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-06-23 18:36:10 +0100

    tests for state priors

---
+++ test/statepriors.jl
@@ -0,0 +1,33 @@
+using Particles: ChineseRestaurantProcess, marginal_log_prior, candidates
+
+@testset "Priors on states" begin
+    @testset "Chinese restaurant process" begin
+
+        crp = ChineseRestaurantProcess(0.5)
+        @test crp.N == Float64[]
+        @test crp.α == 0.5
+
+        @test candidates(crp) == 1:1
+
+        crp1 = add(crp, 1)
+        @test crp1.N == [1.]
+        @test crp1.α == crp.α
+        @test candidates(crp1) == 1:2
+
+        
+
+        crp11 = add(crp1, 1)
+        @test crp11.N == [2.]
+
+        crp111 = add(crp11, 1, 0.5)
+        @test crp111.N == [2.5]
+
+        @test_throws BoundsError add(crp, 2)
+        @test_throws BoundsError add(crp1, 3)
+
+        crp1112 = add(crp111, 2)
+        @test crp1112.N == [2.5, 1.]
+        @test candidates(crp1112) == 1:3
+        
+    end
+end
