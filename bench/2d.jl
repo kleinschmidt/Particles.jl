4000e3f56de32a2cb986bd50a215fed6b89ebd15
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-10-02 23:00:43 -0400

    benchmarking using static matrix for covariance

--- bench/2d.jl
+++ bench/2d.jl
@@ -1,6 +1,7 @@
 using BenchmarkTools
 using Particles, Distributions, ConjugatePriors
 using ConjugatePriors: NormalInverseWishart
+using StaticArrays
 using Compat
 using Compat.LinearAlgebra
 
@@ -10,11 +11,28 @@ truth = MixtureModel([MvNormal(-2 .* ones(2), Matrix(1.0I,2,2)),
 y = [rand(truth) for _ in 1:100]
 
 function f(y)
-    ps = ChenLiuParticles(100,
-                          NormalInverseWishart(zeros(2), 0.1, Matrix(1.0I,2,2), 3.0),
-                          ChineseRestaurantProcess(1.))
+    prior = NormalInverseWishart(zeros(2), 0.1, Matrix(1.0I,2,2), 3.0)
+    stateprior = ChineseRestaurantProcess(1.)
+    ps = ChenLiuParticles(100, prior, stateprior)
     filter!(ps, y, false)
 end
 
 f(y)
 @btime(f($y))
+
+
+
+function g(y)
+    prior = NormalInverseWishart(zeros(2), 0.1, cholesky(SMatrix{2,2}(1.0I)), 3.0)
+    stateprior = ChineseRestaurantProcess(1.)
+    ps = ChenLiuParticles(100, prior, stateprior)
+    filter!(ps, y, false)
+end
+
+gps = g(y)
+@btime(g($y))
+
+
+sy = SVector{2}.(y)
+g(sy)
+ps3 = @btime g($sy)
