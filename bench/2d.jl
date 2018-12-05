9be0bf54241563acf754b6d7f498bbad43ae0279
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-05 11:41:31 -0500

    more static arrays in 2d benchmark

--- bench/2d.jl
+++ bench/2d.jl
@@ -1,8 +1,8 @@
-using BenchmarkTools, 
-    Particles, 
-    Distributions, 
+using BenchmarkTools,
+    Particles,
+    Distributions,
     ConjugatePriors,
-    StaticArrays, 
+    StaticArrays,
     Compat,
     Random
 
@@ -15,29 +15,22 @@ truth = MixtureModel([MvNormal(-2 .* ones(2), Matrix(1.0I,2,2)),
                       MvNormal(2 .* ones(2), Matrix(1.0I,2,2))])
 y = [rand(truth) for _ in 1:100]
 
-function f(y)
-    prior = NormalInverseWishart(zeros(2), 0.1, Matrix(1.0I,2,2), 3.0)
+prior = NormalInverseWishart(zeros(2), 0.1, Matrix(1.0I,2,2), 3.0)
+
+function f(y, prior)
     stateprior = ChineseRestaurantProcess(1.)
     ps = ChenLiuParticles(100, prior, stateprior)
     filter!(ps, y, false)
 end
 
-f(y)
-@btime(f($y))
+f(y, prior)
+@btime(f($y, $prior))
 
+sprior = NormalInverseWishart(SVector{2}(zeros(2)), 0.1, cholesky(SMatrix{2,2}(1.0I)), 3.0)
 
-
-function g(y)
-    prior = NormalInverseWishart(zeros(2), 0.1, cholesky(SMatrix{2,2}(1.0I)), 3.0)
-    stateprior = ChineseRestaurantProcess(1.)
-    ps = ChenLiuParticles(100, prior, stateprior)
-    filter!(ps, y, false)
-end
-
-gps = g(y)
-@btime(g($y))
-
+f(y, sprior)
+@btime(f($y, $sprior))
 
 sy = SVector{2}.(y)
-g(sy)
-ps3 = @btime g($sy)
+f(sy, sprior)
+ps3 = @btime f($sy, $sprior)
